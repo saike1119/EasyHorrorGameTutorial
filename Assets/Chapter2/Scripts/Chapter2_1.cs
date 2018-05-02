1aa15b6c991393fd5c6876eaa80774f0c2663341
saike1119 <saike1119@gmail.com>
2018-05-02 17:52:18 +0900

    Chapter2ファイル追加

---
+++ Assets/Chapter2/Scripts/Chapter2_1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter2_1 : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		Hello ();
+		CallName ("太郎");
+		Debug.Log (Add (100, 10));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void Hello ()
+	{
+		Debug.Log ("こんにちは!");
+	}
+
+	string Hello (string n)
+	{
+		return n;
+	}
+
+	void CallName (string name)
+	{
+		Debug.Log ("こんにちは" + name);
+	}
+
+	int Add (int x, int y)
+	{
+		int z = x + y;
+		return z;
+	}
+}
---
+++ Assets/Chapter2/Scripts/Chapter2_2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter2_2 : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+		Player p = new Player ();
+		p.Attack ();
+		p.Damage (p.GetPower ());
+		p.Damage (p.GetPower ());
+	}
+}
+
+public class Player
+{
+	private int hp = 100;
+	private int power = 50;
+
+	public void Attack ()
+	{
+		Debug.Log ("敵の攻撃力は" + this.power);
+	}
+
+	public void Damage (int damage)
+	{
+		this.hp -= damage;
+		Debug.Log (damage + "を受けた");
+		if (this.hp <= 0) {
+			Debug.Log ("今のHPは" + this.hp + "。死んでしまった");
+		} else {
+			Debug.Log ("今のHPは" + this.hp);
+		}
+	}
+
+	public int GetPower ()
+	{
+		return this.power;
+
+	}
+}
