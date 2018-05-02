05be1aaf0151f0c9fcb4d2e3a5d943e805e563b8
saike1119 <saike1119@gmail.com>
2018-05-02 16:55:27 +0900

    Chapter1追加

---
+++ Assets/Chapter1/Sample4.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample4: MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		int[] arr = new int[5];
+		int[] points = { 10, 58, 49, 30, 20 };
+		int sum = 0;
+		int ave = 0;
+
+		for (int i = 0; i < points.Length; i++) {
+			Debug.Log (points [i]);
+			sum += points [i];
+		}
+
+		ave = sum / points.Length;
+		Debug.Log ("平均点は"+ave);
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
