558f5ebf31f1543511e40c7347caf9ecd6eaa251
saike1119 <saike1119@gmail.com>
2018-05-07 00:19:06 +0900

    Chapter6,7追加

---
+++ Assets/Chapter7/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+
+	public Transform target;
+	static Vector3 pos;
+	private Animator animator;
+
+	float agentToPatroldistance;
+	float agentToTargetdistance;
+
+	UnityEngine.AI.NavMeshAgent agent;
+
+	void Awake ()
+	{
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		animator = GetComponent<Animator> ();
+	}
+
+	void Update ()
+	{
+		agentToPatroldistance = Vector3.Distance (this.agent.transform.position, pos);
+		agentToTargetdistance = Vector3.Distance (this.agent.transform.position, target.transform.position);
+		DoTracking ();
+	}
+
+	public void  DoTracking ()
+	{
+		pos = target.position; 
+		agent.SetDestination (pos);
+	}
+}
