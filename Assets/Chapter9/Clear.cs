f8755cdd1c374463f396183fd7cbc2f0a88ff2c1
saike1119 <saike1119@gmail.com>
2018-05-16 07:49:26 +0900

    Chapter9追加

---
+++ Assets/Chapter9/Clear.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Clear : MonoBehaviour
+{
+	public GameObject key;
+	private bool opened;
+	private bool isNear;
+	public AudioClip close;
+
+	void Start ()
+	{
+		opened = false;
+		isNear = false;
+	}
+
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Fire1") && isNear && key.active == true) {
+			Debug.Log ("鍵がかかっている！");
+			GetComponent<AudioSource> ().PlayOneShot (close);
+		}
+
+
+		if (Input.GetButtonDown ("Fire1") && isNear && key.active == false) {
+			if (!opened) {
+				SceneManager.LoadScene ("Clear");
+				Debug.Log ("車が動いた！");
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider theCollider)
+	{
+		if (theCollider.tag == "Player") {
+			isNear = true;
+		}
+	}
+
+	void OnTriggerExit (Collider theCollider)
+	{
+		if (theCollider.tag == "Player") {
+			isNear = false;
+		}
+	}
+}
---
+++ Assets/Chapter9/MainMenu1.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu1 : MonoBehaviour
+{
+
+	public bool quit = false;
+
+	void Start ()
+	{
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.Confined;
+	}
+
+	void OnMouseEnter ()
+	{
+
+		GetComponent<Renderer> ().material.color = Color.red;
+
+	}
+
+	void OnMouseExit ()
+	{
+
+		GetComponent<Renderer> ().material.color = Color.white;
+
+	}
+
+	void OnMouseUp ()
+	{
+
+		if (quit == true) {
+			Application.Quit ();
+			Debug.Log ("quitしました");
+		} else {
+			SceneManager.LoadScene ("Chapter9");
+		}
+
+	}
+}
