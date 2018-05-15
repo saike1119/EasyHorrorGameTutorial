09dfe0d7cce1bc3cc1c6a8c559b39a2d3e4eb5dc
saike1119 <saike1119@gmail.com>
2018-05-16 07:49:40 +0900

    Chapter10追加

---
+++ Assets/Chapter10/MainMenuLoad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLoad : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine (MainManuLoad ());
+	}
+
+	IEnumerator MainManuLoad ()
+	{
+		yield return new WaitForSeconds (5f);
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
